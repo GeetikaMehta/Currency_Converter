43fae02ab430e7e334f5d3c61719ad83d1ea163f
Geetika Mehta <geetikamehta777@gmail.com>
2025-07-28 14:38:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Converter from './Converter';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Currency Converter</h1>
+      <Converter />
+    </div>
+  );
+}
+
+export default App;
